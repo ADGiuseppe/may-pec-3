2420df6096589ed74983391f7e7ce9daebadb65e
Alice Di Giuseppe <adigiuseppe@uoc.edu>
2025-06-04 18:29:42 +0200

    apply atomic css and tailwind libraries, delete obsolete css

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,36 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#386641',
+        'primary-light': '#829e72a5',
+        'primary-dark': '#2a4733',
+        secondary: '#f2e8cf',
+        tertiary: '#bc4749',
+        accent: '#a7c957',
+        'heading-color': 'var(--bs-heading-color)',
+      },
+      fontFamily: {
+        sans: [
+          '-apple-system',
+          'BlinkMacSystemFont',
+          '"Segoe UI"',
+          'Roboto',
+          '"Helvetica Neue"',
+          'Arial',
+          'sans-serif',
+          '"Apple Color Emoji"',
+          '"Segoe UI Emoji"',
+          '"Segoe UI Symbol"',
+        ],
+        serif: [
+          'Georgia',
+          '"Times New Roman"',
+          'serif',
+        ],
+      },
+    },
   },
   plugins: [],
 }
